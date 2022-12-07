9cd62532e3b6c2cdcd3b92f0300b8ca688b1c2d5
Eric K <erickirberger@gmail.com>
2022-12-06 22:28:16 -0500

    got the api key to work with a given city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,17 @@
-// `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`
-
-var API_KEY =  '3044316f6126db93462603440b6cd43c';
-
-var API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=${API_KEY}`;
-
-// var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${API_KEY}`;
+var apiKey =  '3044316f6126db93462603440b6cd43c';
 
 var cities = [];
 
-
 function displayChosenCity() {
 
-  var cityName = $(this).attr('data-name');
+  var cityName = $('#search-input').val().trim();
 
-  var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${API_KEY}`;
+  var apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`
+
   console.log(`You have searched for this city: ${cityName}`);
 
-  // var movie = $(this).attr('data-name');
-
   $.ajax({
-    url: queryURL,
+    url: apiURL,
     method: "GET"
   }).then(function(response) {
     console.log(response);
@@ -27,6 +19,24 @@ function displayChosenCity() {
   })
 
 };
+
+// function renderCards() {
+// }
+
+$('#search-btn').on('click', function(event) {
+  event.preventDefault();
+  console.log('button is working');
+
+  var cityName = $('#search-input').val().trim();
+
+  cities.push(cityName);
+  
+  // renderCards();
+});
+
+// $(document).on('click', '#search-btn #search-history', displayChosenCity);
+$(document).on('click', '#search-btn', displayChosenCity);
+
 
 // function fiveDay() {
 //   fetch({
@@ -55,17 +65,6 @@ function displayChosenCity() {
 
 
 
-('#search-location').on('click', function(event) {
-  event.preventDefault();
-  console.log('this clicked');
-  // create a variable to grab the user VALUE fromt the input box
-  var cityName = $('#search-location').val().trim();
-  // add a movie to my array
-  cities.push(cityName);
-  // every time we add a movie to the array we want to rerender the buttons
-  displayChosenCity();
-
-});
 
 // $('#search-city').on('click', function(event) {
 //   event.preventDefault;
@@ -78,7 +77,6 @@ function displayChosenCity() {
 //   displaySearchHistory();
 // })
 
-$(document).on('click', '#search-location #search-history', displayChosenCity)
 
 // function render() {
 //   displayCity();
